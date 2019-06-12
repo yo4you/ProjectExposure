cc5a455da27c9686c1a693a008149ed4c8aa1951
yo4you <yo4you.nl@gmail.com>
2019-06-12 23:37:48 +0200

    questionable build code, also level indicators

---
+++ Assets/LevelCounter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCounter : MonoBehaviour
+{
+	[SerializeField]
+	GameObject _levelEnd;
+	private void Start()
+	{
+		var lvl = PlayerPrefs.GetInt("level");
+		SetVisible(lvl);
+		if (lvl == 3)
+		{
+			_levelEnd.SetActive(true);
+		};
+	}
+	private void OnApplicationQuit()
+	{
+		PlayerPrefs.SetInt("level",0);
+	}
+	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+	{
+		var lvl = PlayerPrefs.GetInt("level");
+		SetVisible(lvl);
+		if (lvl == 3)
+		{
+
+		}
+	}
+
+	void SetVisible(int toEnable)
+	{
+		int i = 0;
+		foreach (Transform child in transform)
+		{
+			child.gameObject.SetActive(i == toEnable);
+			i++;
+
+		}
+	}
+
+}
