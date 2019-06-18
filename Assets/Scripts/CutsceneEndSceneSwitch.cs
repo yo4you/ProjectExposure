3446c6f4df3f87f01d42fa140dc9766cdffc46fb
yo4you <yo4you.nl@gmail.com>
2019-06-18 09:53:32 +0200

    skippable scene

---
+++ Assets/Scripts/CutsceneEndSceneSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class CutsceneEndSceneSwitch : MonoBehaviour
+{
+	private PlayableDirector _director;
+	[SerializeField]
+	int _scene;
+
+	void Start()
+    {
+		_director = FindObjectOfType<PlayableDirector>();
+	}
+
+    void Update()
+    {
+		if (_director.state == PlayState.Paused)
+		{
+			SceneManager.LoadScene(_scene);
+		}
+    }
+}
