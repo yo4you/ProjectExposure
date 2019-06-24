912961f73f48fcca7d0fc9dd0441b28ff3f5d37d
yo4you <yo4you.nl@gmail.com>
2019-06-24 16:04:42 +0200

    bouncing guide

---
+++ Assets/FeebackSubmission.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeebackSubmission : MonoBehaviour
+{
+	[SerializeField]
+	bool _learn;
+
+	public void SubmitFeedback(int feedback)
+	{
+
+	}
+
+    
+}
---
+++ Assets/ShakingCompass.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ShakingCompass : MonoBehaviour
+{
+	private Animator _anim;
+	private NodeTransverser _player;
+	private Exit _exit;
+	private float _lastDist;
+
+
+	private void Start()
+	{
+		_anim = GetComponent<Animator>();
+		FindObjectOfType<NodeTransverser>().OnPathResolve += Step;
+
+	}
+
+	private void Step()
+	{
+		_exit = _exit ?? FindObjectOfType<Exit>();
+		_player = _player ?? FindObjectOfType<NodeTransverser>();
+		if (_exit == null)
+		{
+			return;
+		}
+
+		var dist = Vector2.Distance(_player.transform.position, _exit.transform.position);
+		if (_lastDist == dist)
+		{
+			return;
+		}
+		if (_lastDist < dist)
+		{
+			_anim.SetBool("Shaking", true);
+
+		}
+		else
+		{
+			_anim.SetBool("Shaking", false);
+		}
+		_lastDist = dist;
+
+	}
+
+}
